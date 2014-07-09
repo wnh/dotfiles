8d567fe211241ed80a7956cb768f4323b551aad0
Will Harding <harding.will@gmail.com>
2014-07-09 12:52:32 -0700

    Initial commmit with install script

---
+++ _install.sh
@@ -0,0 +1,25 @@
+
+IGNORE="_install.sh"
+
+echo "Installing dot files..."
+
+for src in *; do
+  dst="~/.$src"
+
+
+  if echo "$IGNORE" | grep -q "$src"; then
+    echo "Ignoring $src"
+    continue
+  fi
+
+  # Backup old files
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]; then
+    echo "Backup: $dst -> ${dst}.back"
+    mv "$dst" "${dst}.back"
+  fi
+
+  # Link new guys into place.
+  echo "Linking $dst"
+  #ln -s "$src" "$dst"
+  echo 'ln ' "$src" "$dst"
+done
---
+++ bashrc
@@ -0,0 +1,6 @@
+
+for f in ~/.bashrc.d/*; do
+  source "$f"
+done
+
+
